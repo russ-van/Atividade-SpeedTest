14aef04d0be6eb255fe9d69fae18f1327f7b1be6
Russvan <estudante.brasilia23@gmail.com>
2023-09-21 01:30:27 -0300

    first commit

--- JS/st01_03.js
+++ JS/st01_03.js
@@ -1,6 +1,7 @@
 function cadastrarPedido() {
     let nome = document.querySelector("#nome").value;
     let mesa = parseInt(document.querySelector("#mesa").value);
+<<<<<<< HEAD
     let comanda = parseInt(document.querySelector("#comanda").value);
     let total = parseFloat(document.querySelector("#total").value);
     let gorjeta = parseFloat( total + (total * 0.10));
@@ -13,4 +14,24 @@ function cadastrarPedido() {
 
     document.querySelector("#ficha").innerHTML = `Nome: ${nome} <br> Mesa: ${mesa} <br> Comanda: ${comanda} <br> Total do pedido: ${total} <br> Gorjeta 10%: ${gorjeta}`;
     
+=======
+    let total = parseFloat(document.querySelector("#total").value);
+    let gorjeta = (total * 0.10);
+    let totalGorjeta = total + gorjeta
+
+    localStorage.setItem("nome", nome);
+    localStorage.setItem("mesa", mesa);
+    localStorage.setItem("total", (total).toFixed(2));
+    localStorage.setItem("gorjeta", (gorjeta).toFixed(2));
+    localStorage.setItem("totalGorjeta", (totalGorjeta).toFixed(2));
+
+    document.querySelector("#ficha").innerHTML = `
+    <ul>        <li>Garçom: ${localStorage.getItem("nome")}</li>
+                <li>Mesa: ${localStorage.getItem("mesa")}</li>
+                <li>Gastos: R$ ${localStorage.getItem("total")}</li>
+                <li>Gorjeta (10%): R$ ${localStorage.getItem("gorjeta")}</li>
+                </ul>
+                <p style="font-size: 135%; margin-left: 3vh;">Fatura total: R$ ${localStorage.getItem("totalGorjeta")}</p>`
+
+>>>>>>> 2e7b7e8 (first commit)
 }
