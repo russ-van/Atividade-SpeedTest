5b3edf508ed4efea034476f77c49baa1c5cd834a
Russvan <estudante.brasilia23@gmail.com>
2023-09-21 01:36:08 -0300

    first commit

--- JS/st01_03.js
+++ JS/st01_03.js
@@ -1,20 +1,5 @@
 function cadastrarPedido() {
-    let nome = document.querySelector("#nome").value;
-    let mesa = parseInt(document.querySelector("#mesa").value);
-<<<<<<< HEAD
-    let comanda = parseInt(document.querySelector("#comanda").value);
-    let total = parseFloat(document.querySelector("#total").value);
-    let gorjeta = parseFloat( total + (total * 0.10));
-
-    localStorage.setItem("nome", nome);
-    localStorage.setItem("mesa", mesa);
-    localStorage.setItem("comanda", comanda);
-    localStorage.setItem("total", total);
-    localStorage.setItem("gorjeta", gorjeta);
-
-    document.querySelector("#ficha").innerHTML = `Nome: ${nome} <br> Mesa: ${mesa} <br> Comanda: ${comanda} <br> Total do pedido: ${total} <br> Gorjeta 10%: ${gorjeta}`;
-    
-=======
+ 
     let total = parseFloat(document.querySelector("#total").value);
     let gorjeta = (total * 0.10);
     let totalGorjeta = total + gorjeta
@@ -33,5 +18,4 @@ function cadastrarPedido() {
                 </ul>
                 <p style="font-size: 135%; margin-left: 3vh;">Fatura total: R$ ${localStorage.getItem("totalGorjeta")}</p>`
 
->>>>>>> 2e7b7e8 (first commit)
 }
